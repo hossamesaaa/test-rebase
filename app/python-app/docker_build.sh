0fdfb86d3a56548ccb79179973adead6993f8971
hossamesaaa <hossamesaa2019@gmail.com>
2023-10-26 10:36:51 +0200

    add docker script2

--- app/python-app/docker_build.sh
+++ app/python-app/docker_build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-docker build . -t asia-south2-docker.pkg.dev/hossam-eissa-project/docker-repository/python:v1
+sudo docker build . -t asia-south2-docker.pkg.dev/hossam-eissa-project/docker-repository/python:v1
 
 gcloud auth print-access-token \
-    --impersonate-service-account sa-private-vm@hossam-eissa-project.iam.gserviceaccount.com | docker login \
+    --impersonate-service-account sa-private-vm@hossam-eissa-project.iam.gserviceaccount.com | sudo docker login \
     -u oauth2accesstoken \
     --password-stdin https://asia-south2-docker.pkg.dev
 
-docker push asia-south2-docker.pkg.dev/hossam-eissa-project/docker-repository/python:v1
+sudo docker push asia-south2-docker.pkg.dev/hossam-eissa-project/docker-repository/python:v1
 
 # gcloud config set project hossam-eissa-project
 # gcloud builds submit --region=asia-south2 --config cloudbuild.yaml
