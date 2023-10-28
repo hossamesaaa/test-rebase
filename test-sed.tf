2e0820e5ccb69321531e7b50707dbe42795f2a48
hossamesaaa <hossamesaa2019@gmail.com>
2023-10-28 11:44:18 +0200

    add approval stage

--- test-sed.tf
+++
@@ -1,9 +0,0 @@
-master_authorized_networks_config {
-    cidr_blocks {
-      display_name = "My Management Subnet"
-      cidr_block   = module.network-mod.management_subnet_cidr
-    }
-     cidr_blocks {
-      display_name = "My_ip"
-      cidr_block   = "196.157.99.41/32"
-    }
